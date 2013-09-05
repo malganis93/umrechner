437be5e92b27516250d237bd747e86b063436255
Lars <Lars@SurfacePro.fritz.box>
2013-09-05 22:01:12 +0200

    added eclipse files

--- Umrechnung/src/de/browserlife/umrechner/Umrechner.java
+++ Umrechnung/src/de/browserlife/umrechner/Umrechner.java
@@ -10,6 +10,7 @@ public class Umrechner {
 		System.out.println("Dieses Programm rechnet jede Zahl in Binaer, Oktal und Hexadezimal!");
 		System.out.println("Welche Zahl moechtest du Umrechnen?");
 		Scanner sc = new Scanner(System.in);
+		
 		int iDez = sc.nextInt();
 		Converter conv = new Converter();
 		
@@ -24,5 +25,27 @@ public class Umrechner {
 		conv.setBase(16);
 		System.out.println("Hexadezimal:");
 		System.out.println(conv.convert(iDez));
+		
+		while (true)
+		{
+			System.out.println("Moechtest du die Zahl in noch ein Zahlensystem umwandeln? [ja/nein]");
+			String answ = sc.next();
+			
+			if (answ.equals("ja"))
+			{
+				System.out.println("Welche Basis hat das Zahlensystem?");
+				int zs = sc.nextInt();
+				if (zs<2 || zs>35)
+				{
+					System.out.println("Nicht unterstütztes Zahlensystem");
+					continue;
+				}
+								
+				conv.setBase(zs);
+				System.out.println("Zur Basis "+zs+":");
+				System.out.println(conv.convert(iDez));
+			} else {System.exit(0);}
+		}
+		
 	}
 }
